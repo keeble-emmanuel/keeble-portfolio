bc8badcea2d74bf31332d9d9c49810d155fa9b09
Emmanuel kunsanama <158379900+keeble-emmanuel@users.noreply.github.com>
2024-03-27 00:59:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const wasap = document.getElementById("wasap");
+
+function nothingToShow(){
+    alert("to contact me on WhatsApp you need a WhatsApp application. use this number number to contact me: 00996572804")
+}
+
+wasap.addEventListener('click', nothingToShow)
